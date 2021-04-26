938aa1a52010d9b9bc3e91c01ea4667636dad81e
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-04-26 18:33:38 +0200

    Add background image on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-export default function Home() {
+export default function Home(props) {
+  const { content } = props;
+
   return (
-    <Fragment>
-      <h1>Home</h1>
-    </Fragment>
+    <Container backgroundImage={content.home.images.background.src}>
+      <div>Home</div>
+    </Container>
   );
 }
+
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  width: 100%;
+  position: relative;
+  top: 70px;
+  padding: 15px;
+  box-sizing: border-box;
+
+  &:after {
+    background-image: url(${(props) => props.backgroundImage});
+    background-size: cover;
+    background-attachment: fixed;
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`;
