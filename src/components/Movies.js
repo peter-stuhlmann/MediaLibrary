034495d38023a20b5aa23ad995e60b76c99b4b0f
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-04-30 18:14:32 +0200

    Add slider

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Slider from 'react-slick';
 
 export default function Movies(props) {
   const { title, movies } = props;
 
+  const settings = {
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
+
   return (
     <Container>
       <h3>{title}</h3>
-      <List>
+      <Carousel {...settings}>
         {movies?.map((movie) => (
           <ListItem>
             <Link to={movie.href}>
@@ -20,25 +44,89 @@ export default function Movies(props) {
             </Link>
           </ListItem>
         ))}
-      </List>
+      </Carousel>
     </Container>
   );
 }
 
 const Container = styled.div`
   padding: 25px;
+
+  @media (max-width: 480px) {
+    padding: 0;
+  }
 `;
 
-const List = styled.ul`
-  display: flex;
-  padding: 0;
-  margin: 0;
-  list-style-type: none;
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+
+  .slick-list {
+    overflow: initial;
+    margin: 0 -12.5px;
+
+    @media (max-width: 480px) {
+      margin: 0 -8px;
+    }
+  }
+
+  .slick-slide > div {
+    margin: 0 12.5px;
+
+    @media (max-width: 480px) {
+      margin: 0 8px;
+    }
+  }
+
+  .slick-prev {
+    width: 40px;
+    left: -40px;
+
+    @media (max-width: 480px) {
+      width: 35px;
+      left: -17.5px;
+    }
+
+    &:before {
+      font-size: 35px;
+
+      @media (max-width: 480px) {
+        font-size: 25px;
+      }
+    }
+  }
+
+  .slick-next {
+    width: 40px;
+    right: -40px;
+
+    @media (max-width: 480px) {
+      width: 35px;
+      right: -17.5px;
+    }
+
+    &:before {
+      font-size: 35px;
+
+      @media (max-width: 480px) {
+        font-size: 25px;
+      }
+    }
+  }
 `;
 
 const ListItem = styled.li`
   flex: 0 0 calc(25% - 18.75px);
-  margin-right: 25px;
   box-shadow: rgb(0 0 0 / 70%) 0px 25px 30px -10px,
     rgb(0 0 0 / 75%) 0px 15px 10px -10px;
   cursor: pointer;
@@ -48,6 +136,17 @@ const ListItem = styled.li`
   border: 3px solid rgba(250, 250, 250, 0.1);
   border-radius: 10px;
   box-sizing: border-box;
+
+  button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  &.slick-active button:before {
+    color: #fff;
+  }
 
   img {
     height: 100%;
